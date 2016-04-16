5aa27d31daa7376e21817155a9c7546f049836e1
Michael <Michael@MichaelHumiston.com>
2016-04-16 16:50:39 +1000

    Further style and center links section

--- src/components/home/Links.js
+++ src/components/home/Links.js
@@ -1,56 +1,64 @@
 const Links = () => (
   <div className="social-links">
-  <h1>Links</h1>
-    <div className="link-container github">
-      <div className="icon">
-        <a href="https://github.com/mhum" target="_blank">
-          <i className="fa fa-github fa-3x" aria-hidden="true"></i>
-        </a>
-      </div>
-      <div className="link">
-        <a href="https://github.com/mhum" target="_blank">
-          https://github.com/mhum
-        </a>
-      </div>
-    </div>
+    <h1>Links</h1>
+    <div className="link-container">
 
-    <div className="link-container linkedin">
-      <div className="icon">
-        <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
-          <i className="fa fa-linkedin fa-3x" aria-hidden="true"></i>
-        </a>
-      </div>
-      <div className="link">
-        <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
-          https://linkedin.com/in/michaelhumiston
-        </a>
-      </div>
-    </div>
+      <div className="github">
+        <div className="icon">
+          <a href="https://github.com/mhum" target="_blank">
+            <i className="fa fa-github fa-3x" aria-hidden="true"></i>
+          </a>
+        </div>
 
-    <div className="link-container flickr">
-      <div className="icon">
-        <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
-          <i className="fa fa-flickr fa-3x" aria-hidden="true"></i>
-        </a>
+        <div className="link">
+          <a href="https://github.com/mhum" target="_blank">
+            https://github.com/mhum
+          </a>
+        </div>
       </div>
-      <div className="link">
-        <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
-          https://www.flickr.com/photos/michaelhumiston
-        </a>
-      </div>
-    </div>
 
-    <div className="link-container facebook">
-      <div className="icon">
-        <a href="https://www.facebook.com/michaelhumiston" target="_blank">
-          <i className="fa fa-facebook fa-3x" aria-hidden="true"></i>
-        </a>
+      <div className="linkedin">
+        <div className="icon">
+          <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
+            <i className="fa fa-linkedin fa-3x" aria-hidden="true"></i>
+          </a>
+        </div>
+
+        <div className="link">
+          <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
+            https://linkedin.com/in/michaelhumiston
+          </a>
+        </div>
       </div>
-      <div className="link">
-        <a href="https://www.facebook.com/michaelhumiston" target="_blank">
-          https://www.facebook.com/michaelhumiston
-        </a>
+
+      <div className="flickr">
+        <div className="icon">
+          <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
+            <i className="fa fa-flickr fa-3x" aria-hidden="true"></i>
+          </a>
+        </div>
+
+        <div className="link">
+          <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
+            https://www.flickr.com/photos/michaelhumiston
+          </a>
+        </div>
       </div>
+
+      <div className="facebook">
+        <div className="icon">
+          <a href="https://www.facebook.com/michaelhumiston" target="_blank">
+            <i className="fa fa-facebook fa-3x" aria-hidden="true"></i>
+          </a>
+        </div>
+
+        <div className="link">
+          <a href="https://www.facebook.com/michaelhumiston" target="_blank">
+            https://www.facebook.com/michaelhumiston
+          </a>
+        </div>
+      </div>
+
     </div>
   </div>
 );
