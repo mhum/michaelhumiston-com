818769d4b87a2724f2deb8645f8addaf8e175b11
Michael <Michael@MichaelHumiston.com>
2016-04-16 15:35:58 +1000

    Add links, format, and style
    TODO: Move styles out of HTML page

--- src/components/home/Links.js
+++ src/components/home/Links.js
@@ -1,6 +1,57 @@
 const Links = () => (
-  <div>
-LINKS
+  <div className="social-links">
+  <h1>Links</h1>
+    <div className="link-container github">
+      <div className="icon">
+        <a href="https://github.com/mhum" target="_blank">
+          <i className="fa fa-github fa-3x" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className="link">
+        <a href="https://github.com/mhum" target="_blank">
+          https://github.com/mhum
+        </a>
+      </div>
+    </div>
+
+    <div className="link-container linkedin">
+      <div className="icon">
+        <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
+          <i className="fa fa-linkedin fa-3x" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className="link">
+        <a href="https://linkedin.com/in/michaelhumiston" target="_blank">
+          https://linkedin.com/in/michaelhumiston
+        </a>
+      </div>
+    </div>
+
+    <div className="link-container flickr">
+      <div className="icon">
+        <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
+          <i className="fa fa-flickr fa-3x" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className="link">
+        <a href="https://www.flickr.com/photos/michaelhumiston" target="_blank">
+          https://www.flickr.com/photos/michaelhumiston
+        </a>
+      </div>
+    </div>
+
+    <div className="link-container facebook">
+      <div className="icon">
+        <a href="https://www.facebook.com/michaelhumiston" target="_blank">
+          <i className="fa fa-facebook fa-3x" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className="link">
+        <a href="https://www.facebook.com/michaelhumiston" target="_blank">
+          https://www.facebook.com/michaelhumiston
+        </a>
+      </div>
+    </div>
   </div>
 );
 
