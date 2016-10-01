e702b1f2cc291bd440aa06e6b1735ff8f9ef233b
Michael <Michael@MichaelHumiston.com>
2016-10-01 22:39:09 +1000

    Add Projects link and refactor link creation

--- src/components/layout/header/Navbar.jsx
+++ src/components/layout/header/Navbar.jsx
@@ -1,22 +1,53 @@
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
-const Navmenu = () => (
+const generateLink = ((link, i) => {
+  const LinkClass = link.isIndex ? IndexLinkContainer : LinkContainer;
+  return (
+    <LinkClass to={link.uri} key={i}>
+      <NavItem eventKey={i}>{link.name}</NavItem>
+    </LinkClass>
+  );
+});
+
+const Navmenu = ({ links }) => (
   <Navbar id="navbar">
     <Navbar.Header>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <IndexLinkContainer to="/">
-          <NavItem eventKey={1}>Home</NavItem>
-        </IndexLinkContainer>
-        <LinkContainer to="about">
-          <NavItem eventKey={2}>About Me</NavItem>
-        </LinkContainer>
+        {
+          links.map((v, i) =>
+            generateLink(v, i)
+          )
+        }
       </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
+
+Navmenu.propTypes = {
+  links: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+};
+
+Navmenu.defaultProps =
+{
+  links: [
+    {
+      uri: '/',
+      name: 'Home',
+      isIndex: true
+    }, {
+      uri: 'projects',
+      name: 'Projects',
+      isIndex: false
+    }, {
+      uri: 'about',
+      name: 'About',
+      isIndex: false
+    }
+  ]
+};
 
 export default Navmenu;
