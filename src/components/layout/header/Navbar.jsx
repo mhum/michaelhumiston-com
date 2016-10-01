f0a779e0e2aca6deacb1dca389efafa63d867c7b
Michael <Michael@MichaelHumiston.com>
2016-10-01 13:57:48 +1000

    Update links and styling

--- src/components/layout/header/Navbar.jsx
+++ src/components/layout/header/Navbar.jsx
@@ -1,17 +1,21 @@
-import { Navbar } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 const Navmenu = () => (
   <Navbar id="navbar">
     <Navbar.Header>
-      <LinkContainer to={{ pathname: '/' }}>
-        <Navbar.Link>Home</Navbar.Link>
-      </LinkContainer>
-      <LinkContainer to={{ pathname: '/about' }}>
-        <Navbar.Link>About</Navbar.Link>
-      </LinkContainer>
       <Navbar.Toggle />
     </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <IndexLinkContainer to="/">
+          <NavItem eventKey={1}>Home</NavItem>
+        </IndexLinkContainer>
+        <LinkContainer to="about">
+          <NavItem eventKey={2}>About</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
 
