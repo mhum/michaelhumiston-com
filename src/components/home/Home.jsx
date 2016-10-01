df2e9ddbd4cc8667be32efd5bab369796319a5e5
Michael <Michael@MichaelHumiston.com>
2016-10-01 16:36:15 +1000

    Update page title when navigating

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,20 +2,31 @@ import { Col, Image, Row } from 'react-bootstrap';
 
 import SocialLinks from './SocialLinks';
 
-const Home = () =>
-  <Row>
-    <Col xs={7}>
-      <Image src="/assets/images/springbrook.jpg" responsive />
-    </Col>
-    <Col xs={5}>
-      <p>
-        Thanks for stopping by my personal website. Links to some of my social media offerings
-        can be found below.
-      </p>
-      <SocialLinks />
-    </Col>
-  </Row>;
+const pageTitle = 'Home';
 
-Home.title = 'Home';
+class Home extends React.Component {
+  componentDidMount() {
+    document.title = `${pageTitle} - Michael Humiston`;
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col xs={7}>
+          <Image src="/assets/images/springbrook.jpg" responsive />
+        </Col>
+        <Col xs={5}>
+          <p>
+            Thanks for stopping by my personal website. Links to some of my social media offerings
+            can be found below.
+          </p>
+          <SocialLinks />
+        </Col>
+      </Row>
+    );
+  }
+}
+
+Home.title = pageTitle;
 
 export default Home;
