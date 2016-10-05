4efcb6d4b29e0f4086c66581d40722296fcc750a
Michael <Michael@MichaelHumiston.com>
2016-10-05 22:52:40 +1100

    Update About page text
    Resolve #2

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,31 +8,35 @@ class About extends React.Component {
   render() {
     return (
       <Row>
-        <Col xs={3}>
-          <Image src="/assets/images/main.jpg" responsive />
-        </Col>
-        <Col xs={9}>
+        <Col xs={7}>
           <p>
-            Information technology is my calling. I was introduced to technology and the wider
-            Internet at a young age through an old Packard Bell running Windows 3.1. What really lit
-            the fire under me was receiving a <a href="http://en.wikipedia.org/wiki/Casio_BE-300">
+            Information technology has been my calling for a long time. I was introduced to
+            technology and the wider Internet at a young age through an old Packard Bell running
+            Windows 3.1. What really lit the fire under me was receiving a <a href="http://en.wikipedia.org/wiki/Casio_BE-300" target="_blank" rel="noreferrer noopener">
             Casio BE-300</a> and being exposed to the immense possibilities that technology provided
             through software development, tinkering, and communities.
           </p>
           <p>
-            Previously, did enterprise <a href="http://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition">
-            Java</a> web development using a variety of technologies such as <a href="http://spring.io/">Spring</a>
-            , <a href="http://hibernate.org/">Hibernate</a>, <a href="http://grails.org/">Grails</a>,
-            and <a href="http://www.sencha.com/products/extjs/">ExtJS</a> for internal projects. Now, I
-            work as a consultant developing online gambling and betting software.
+            Previously, I did enterprise <a href="http://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition" target="_blank" rel="noreferrer noopener">
+            Java</a> web development using a variety of technologies such as <a href="http://spring.io/" target="_blank" rel="noreferrer noopener">Spring</a>
+            , <a href="http://grails.org/" target="_blank" rel="noreferrer noopener">Grails</a>,
+            and <a href="http://www.sencha.com/products/extjs/" target="_blank" rel="noreferrer noopener">ExtJS</a> for internal projects.
           </p>
           <p>
-            My interests have developed a long way since the days of dial-up and PDAs and include
-            things such as information security, network design, systems integration, and network
-            administration. I love learning new technologies and ways of doing things. I am
-            constantly trying to do new things to improve my skills and broaden my horizons. This
-            website is about my musings, personal interests, and projects.
+            Currently, I work as a consultant developing online gambling and betting software.
+            There, I use a wide-variety of tools and technologies doing everything from requirements
+            analysis to live deployments on a high volume software stack. I develop in languages
+            such as <a href="https://tcl.tk" target="_blank" rel="noreferrer noopener">Tcl</a>
+            , <a href="https://www.ruby-lang.org/en" target="_blank" rel="noreferrer noopener">Ruby</a>
+            , <a href="http://www.java.com/en" target="_blank" rel="noreferrer noopener">Java</a>,
+            and <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="noreferrer noopener">JavaScript</a> using
+            frameworks such as <a href="https://facebook.github.io/react/" target="_blank" rel="noreferrer noopener">React</a>
+            , <a href="https://angularjs.org/" target="_blank" rel="noreferrer noopener">Angular</a>
+            , and <a href="http://spring.io/" target="_blank" rel="noreferrer noopener">Spring</a>.
           </p>
+        </Col>
+        <Col xs={5}>
+          <Image src="/assets/images/about.jpg" responsive />
         </Col>
       </Row>
     );
