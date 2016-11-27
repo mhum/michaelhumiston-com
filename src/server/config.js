1473035d3c47135986c03b12f7a11501b41130e5
Michael <Michael@MichaelHumiston.com>
2016-11-26 22:01:32 -0800

    Add server configs and actually send email with submitted info

---
+++ src/server/config.js
@@ -0,0 +1,19 @@
+const Env = require('node-env-file');
+const Path = require('path');
+
+Env(Path.join(__dirname, './.env'));
+
+const config = {};
+
+config.contact = {};
+config.web = {};
+
+config.contact.user_name = process.env.CONTACT_EMAIL || 'username';
+config.contact.password = process.env.CONTACT_PASS || 'password';
+config.contact.subject = process.env.CONTACT_SUBJ || 'Contact Submission - Michael Humiston';
+config.contact.to = process.env.CONTACT_TO || 'email';
+
+config.web.host = process.env.HOST || 'localhost';
+config.web.port = process.env.PORT || '3000';
+
+module.exports = config;
--- src/server/routes/contact.js
+++ src/server/routes/contact.js
@@ -1,5 +1,37 @@
+const Boom = require('boom');
+const Nodemailer = require('nodemailer');
+
+const Config = require('../config');
+
+const user = Config.contact.user_name;
+const pass = Config.contact.password;
+
 function submitContact(request, reply) {
-  reply('test');
+  const payload = request.payload;
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = Nodemailer.createTransport(`smtps://${user}:${pass}@smtp.gmail.com`);
+
+  // setup e-mail data
+  const mailOptions = {
+    from: `"Contact" <${user}>`,
+    replyTo: payload.email,
+    to: Config.contact.to,
+    subject: Config.contact.subject,
+    html: `<p>Name: ${payload.name}</p>
+    <p>Email: ${payload.email}</p>
+    <p>Message: ${payload.message}</p>`
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      reply(Boom.badRequest('Error sending message'));
+      return console.log(error);
+    }
+    reply('success');
+    return console.log(`Message sent: ${info.response}`);
+  });
 }
 
 module.exports = [
