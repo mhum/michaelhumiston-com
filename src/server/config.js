08a4b17dd5348c9e590555ba18c6acf223b35012
Michael <Michael@MichaelHumiston.com>
2016-12-01 10:26:59 -0800

    Handle captcha submission and validate

--- src/server/config.js
+++ src/server/config.js
@@ -12,6 +12,7 @@ config.contact.user_name = process.env.CONTACT_EMAIL || 'username';
 config.contact.password = process.env.CONTACT_PASS || 'password';
 config.contact.subject = process.env.CONTACT_SUBJ || 'Contact Submission - Michael Humiston';
 config.contact.to = process.env.CONTACT_TO || 'email';
+config.contact.captcha = process.env.CONTACT_CAPTCHA || 'XXXXXX';
 
 config.web.host = process.env.HOST || 'localhost';
 config.web.port = process.env.PORT || '3000';
