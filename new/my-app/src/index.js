a82f11a17ce56e0046151d0b6d7ccf07a17c0048
Michael <Michael@MichaelHumiston.com>
2019-08-24 09:51:25 -0700

    Use create-react-ssr-app

--- new/my-app/src/index.js
+++ new/my-app/src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import express from 'express';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let app = require('./server').default;
+
+if (module.hot) {
+  module.hot.accept('./server', () => {
+    console.log('Server reloading...');
+
+    try {
+      app = require('./server').default;
+    } catch (error) {
+      // Do nothing
+    }
+  });
+}
+
+express()
+  .use((req, res) => app.handle(req, res))
+  .listen(process.env.PORT || 3000, () => {
+    console.log(
+      `React SSR App is running: http://localhost:${process.env.PORT || 3000}`
+    );
+  });
