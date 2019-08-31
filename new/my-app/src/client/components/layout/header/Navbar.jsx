17eeb6f32d3c5a02ff35e3aee57306ad78a8eaac
Michael <Michael@MichaelHumiston.com>
2019-08-31 15:29:15 -0700

    Improve navbar

--- new/my-app/src/client/components/layout/header/Navbar.jsx
+++ new/my-app/src/client/components/layout/header/Navbar.jsx
@@ -3,38 +3,47 @@ import PropTypes from 'prop-types';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
-const generateSubMenu = ((link, idx, expanded, onClickLink, projects) => (
+const generateSubMenu = ((link, idx, expanded, onClickLink, projects, currentPath) => {
+  const dropdownActiveClass = currentPath.startsWith(link.uri) ? 'active':  '';
+
+  return (
     <NavDropdown
       title={link.name}
       id="nav-dropdown"
       active={expanded}
       onToggle={onClickLink}
       key={idx}
+      className={dropdownActiveClass}
     >
       <NavDropdown.Item to={link.uri} eventKey={`${idx}`} as={Link}>
           All
       </NavDropdown.Item>
       <NavDropdown.Divider />
       {
-        projects && projects.map((v, ii) => (
-          <NavDropdown.Item to={v.uri} key={v.id} eventKey={`${idx}.${ii}`} as={Link}>
-              {v.name}
-          </NavDropdown.Item> 
-        ))
+        projects && projects.map((v, ii) => {
+          const dropdownItemActiveClass = v.uri === currentPath ? 'active':  '';
+          return (
+            <NavDropdown.Item to={v.uri} key={v.id} eventKey={`${idx}.${ii}`} as={Link} className={dropdownItemActiveClass}>
+                {v.name}
+            </NavDropdown.Item> 
+          )
+        })
       }
     </NavDropdown>
-)
-);
+  )
+});
 
-const generateLink = ((link, idx, expanded, onClickLink, projects) => {
+const generateLink = ((link, idx, expanded, onClickLink, projects, currentPath) => {
   if (link.hasSubMenu) {
-    return generateSubMenu(link, idx, expanded, onClickLink, projects);
+    return generateSubMenu(link, idx, expanded, onClickLink, projects, currentPath);
   }
 
+  const activeClass = link.uri === currentPath ? 'active':  '';
+
   return ( 
-    <Nav.Link to={link.uri} as={Link} key={idx} className="nav-link">
+    <Nav.Link to={link.uri} as={Link} key={idx} className={`nav-link ${activeClass}`}>
       {link.name}
     </Nav.Link>
   );
@@ -58,15 +67,16 @@ class Navmenu extends React.Component {
   }
 
   render() {
-    const { links, projects } = this.props;
+    const { links, location, projects } = this.props;
     const { expanded } = this.state;
+
     return (
       <Navbar id="navbar" expand="md" collapseOnSelect> 
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
             {
-              links.map((link, idx) => generateLink(link, idx, expanded, this.onClickLink, projects))
+              links.map((link, idx) => generateLink(link, idx, expanded, this.onClickLink, projects, location.pathname))
             }
           </Nav>
         </Navbar.Collapse>
@@ -84,4 +94,4 @@ Navmenu.propTypes = {
   projects: PropTypes.arrayOf(PropTypes.shape({}))
 };
 
-export default Navmenu;
+export default withRouter(Navmenu);
